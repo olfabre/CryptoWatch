338c2aeb7e007c110e8a7fff7551200bd3cb6a97
olfabre <olfabre@gmail.com>
2024-12-10 11:11:23 +0100

    101224A
    Reconfiguration Serveur Distant

--- src/old/lib/core.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once "core/vendor/autoload.php";
-use Doctrine\ORM\Tools\Setup;
-use Doctrine\ORM\EntityManager;
-
-require_once 'core/context.class.php' ;
-require_once 'core/dbconnection.class.php' ;
-
--- src/old/monApplication/view/indexSuccess.php
+++
@@ -1,2 +0,0 @@
-C'est l'action par défaut ! 
-<a href=monApplication.php?action=logout>Deconnectez vous !</a>
